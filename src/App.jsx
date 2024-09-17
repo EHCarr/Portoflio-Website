75dc18a9c3c6ba4fac4db09b5c66f19846dd1b83
EHCarr <ash100.singh@gmail.com>
2024-09-18 00:39:30 +0100

    fixing production failure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 const App = () => {
     return (
       <div className="overflow-x-hidden text-stone 300 antialiased">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,23 @@
 import { email } from "../constants";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-        <h1 className="my-8 text-center text-4xl text-stone-300">Get in Touch</h1>
-        <p className="text-center tracking-tighter text-stone-200">{email}</p>
+        <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-8 text-center text-4xl text-stone-300">
+            Get in Touch
+        </motion.h1>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="text-center tracking-tighter text-stone-200 my-2">
+            {email}
+        </motion.p>       
     </div>
   );
 };
