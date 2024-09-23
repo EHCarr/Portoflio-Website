326ab0876179aed3b172fa7deed00baf72e08d8d
EHCarr <ash100.singh@gmail.com>
2024-09-23 16:37:33 +0100

    changed writing sample ui

--- src/components/WritingSample.jsx
+++ src/components/WritingSample.jsx
@@ -21,26 +21,23 @@ return(
     <div 
     className="pb-20">
         <h2 className="my-5 text-center text-white text-3xl">Writing Samples</h2>
+        <div className="flex flex-row justify-center space-x-5">
             <div className="flex flex-col justify-center items-center mt-4">
-                <div
-                className="my-5 text-center text-white text-l">
-                    Transing the Modern World 
-                </div>
                 <button
                 onClick={handleEssayDownload}
-                className="mt-2 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300">
-                    Download Essay
-                </button>
-                <div
-                className="my-5 text-center text-white text-l">
-                    Sitamahri Murder Crime Report 
-                </div>
-                <button
-                onClick={handleCrimeReportDownload}
-                className="mt-2 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300">
-                    Download Article
+                className="py-2 px-8 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300">
+                    Transing the Modern World
                 </button>
             </div>
+            <div className="flex flex-col justify-center items-center mt-4">
+                <button
+                onClick={handleCrimeReportDownload}
+                className="py-2 px-8 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300">
+                    Sitamarhi Murder Crime Report
+                </button>
+            </div>
+        </div>
+            
     </div>
 )
 } 
